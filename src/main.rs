7dc75b82d41fa30808e681b942b8a046f0786b26
zeljic <zeljic@gmail.com>
2017-02-08 21:43:34 +0100

    Tag and Attr structs are defined. Not very optimized.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,91 @@ use xmltree::Element;
 
 use utils::{log_print, LogLevel};
 
-fn compare_nodes(el_x: &Element, el_y: &Element) {}
+use std::io;
+use std::cmp::Ordering::*;
+
+#[derive(Debug)]
+struct Attr<'attr> {
+    key: &'attr str,
+    value: &'attr str,
+}
+
+impl<'attr> Attr<'attr> {
+    fn new(data: (&'attr str, &'attr str)) -> Attr<'attr> {
+
+        let ret_val: Attr<'attr> = Attr {
+            key: data.0,
+            value: data.1,
+        };
+
+        return ret_val;
+    }
+
+    fn print(&self) -> String {
+        format!("{}=\"{}\"", self.key, self.value)
+    }
+}
+
+#[derive(Debug)]
+struct Tag<'tag> {
+    el: &'tag Element,
+    attrs: Vec<Attr<'tag>>,
+}
+
+impl<'tag> Tag<'tag> {
+    fn new(el: &'tag Element) -> Tag<'tag> {
+
+        let mut ret_val: Tag<'tag> = Tag {
+            el: el,
+            attrs: Vec::new(),
+        };
+
+        for attr in &el.attributes {
+            let attr: Attr<'tag> = Attr::<'tag>::new((attr.0, attr.1));
+            ret_val.attrs.push(attr);
+        }
+
+        if ret_val.attrs.len() > 0 {
+            ret_val.attrs.sort_by(|x, y| if x.key > y.key {
+                Greater
+            } else if x.key < y.key {
+                Less
+            } else {
+                Equal
+            });
+        }
+
+        ret_val
+    }
+
+    fn print(&self) -> String {
+        let mut ret_val: String = String::from("<");
+
+        ret_val.push_str(&self.el.name);
+
+        for attr in &self.attrs {
+            ret_val.push(' ');
+            ret_val.push_str(&attr.print());
+        }
+
+        ret_val.push_str(">");
+
+        ret_val
+    }
+}
+
+fn compare_nodes(el_x: &Element, el_y: &Element) {
+
+    let mut tag_x: Tag = Tag::new(el_x);
+
+    if el_x.children.len() > 0 {
+        for child in &el_x.children {
+            compare_nodes(child, child);
+        }
+    } else {
+
+    }
+}
 
 fn main() {
 
